97d66bca3d76367e7c2f2ab3c7f11daad30810d8
leninrkb <rkb.lenin@gmail.com>
2023-03-10 20:46:18 -0500

    creacion entidad casos covid

---
+++ SolucionCasosCovid/Datos/EntidadCasosCovid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class EntidadCasosCovid
+    {
+        public int Id { get; set; }
+        public string Provincia { get; set; }
+        public int Casos { get; set; }
+        public int Fallecidos { get; set; }
+        public int Vacunados { get; set; }
+        public int CentrosVacunacion { get; set; }
+        public string Digitador { get; set; }
+
+        public EntidadCasosCovid()
+        {
+        }
+
+        public EntidadCasosCovid(int id, string provincia, int casos, int fallecidos, int vacunados, int centrosVacunacion, string digitador)
+        {
+            Id = id;
+            Provincia = provincia;
+            Casos = casos;
+            Fallecidos = fallecidos;
+            Vacunados = vacunados;
+            CentrosVacunacion = centrosVacunacion;
+            Digitador = digitador;
+        }
+    }
+}
